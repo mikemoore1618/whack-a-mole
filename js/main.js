5f0979f2c367ce9fe14b434f15822b596bd8dd66
Mike Moore <mikemoore1618@gmail.com>
2018-05-15 09:07:26 -0700

    final js

--- js/main.js
+++ js/main.js
@@ -15,38 +15,54 @@
 // string literal
 
 
-var button = document.getElementById('start-button');
+const button = document.getElementById('start-button');
 const timerDisplay = document.getElementById('timer');
-const score = document.getElementById('score');
+var score = document.getElementById('score');
 const mole = document.getElementById('mole');
 const holes = document.getElementsByClassName('hole');
+var newScore = 0
+
 
 //function to move mole randomly every second
 //generate random number and move to that hole
-function moleMover() {  
-    holes[Math.floor(Math.random() * 9)]appendChild.mole
-}
-
-setInterval("", 1000)
-function enableMole() {
+function moveMole() {  
+    const hole = holes[Math.floor(Math.random() * 9)];
+    hole.appendChild(mole)
 
 }
+
+//function for what happens when you click mole
+function moleClickHandler() {
+    newScore = newScore + 1
+    score.innerText = newScore + " points"
+}
+
+
+//functiom to start moving mole
+function startMole() {
+    mole.addEventListener("click", moleClickHandler) 
+}
+
 //remove event listener-removes ability to click mole and lock scoreboard
 function disableMole() {
-    mole.removeEventListener("click", moleMover)
+    mole.removeEventListener("click", moleClickHandler)
 }
 
 function startGame() {//create function to start game`
     var timeLeft = 30;//set value of timer to start at 30
+    startMole()
+    var moleInterval = setInterval(moveMole, 1000)
     var timerInterval = setInterval(countDown, 1000); // set countdown interval to 1 second//1000 will run it every 1 second
     function countDown() {// create function for timer
         timeLeft = timeLeft - 1;//set timer to count down by an increment of 1
         if (timeLeft <= 0) { //when timer gets to 0, 
-        clearInterval(timerInterval);//stop timer
-        //disableMole
+            clearInterval(timerInterval), //stop timer 
+            clearInterval(moleInterval);//stop moving mole
+            disableMole()
+            alert(`Game Over! Your Score is ${newScore}`)
         // GAME ENDS HERE
-    }
-    timerDisplay.innerHTML = "00:" + timeLeft;
+        }
+        timerDisplay.innerHTML = "00:" + timeLeft;
     }
 }
 
